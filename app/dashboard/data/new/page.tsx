b1adb94d67e8a622c153545b3a18bd477ac1babe
Varshith-p <varshithpuligadda@gmail.com>
2023-10-04 12:47:11 +0530

    [Feature] Add Upload File Step ui in dashboard

--- app/dashboard/data/new/page.tsx
+++ app/dashboard/data/new/page.tsx
@@ -4,13 +4,12 @@ import Footer from "@/components/Footer";
 import ImageCard from "@/components/ImageCard";
 import React, { useState } from "react";
 
-const steps = [
-  { number: 1, title: "Connection Type" },
-  { number: 2, title: "Upload File" },
-  { number: 3, title: "Select Table" },
-  { number: 4, title: "Edit fields" },
-  { number: 5, title: "Add Dataset Info" },
-];
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import Image from "next/image";
 
 const data = {
   files: [
@@ -42,6 +41,232 @@ const Page = () => {
   const [selectedType, setSelectedType] = useState("");
   const [currentStep, setCurrentStep] = useState(1);
 
+  const steps = [
+    {
+      number: 1,
+      title: "Connection Type",
+      content: (
+        <section className="px-7 py-10 flex flex-col items-start gap-6 pb-36">
+          <div className="flex flex-col items-start gap-6">
+            <p className="text-[17px] font-medium leading-7">Files</p>
+            <div className="flex gap-6 items-start">
+              {data.files.map((file, index) => (
+                <div
+                  key={index}
+                  onClick={() => {
+                    setSelectedType(file.name);
+                  }}
+                >
+                  <ImageCard
+                    image={file.image}
+                    selected={selectedType === file.name}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col items-start gap-6">
+            <p className="text-[17px] font-medium leading-7">Databases</p>
+            <div className="flex gap-6 items-start">
+              {data.databases.map((database, index) => (
+                <div
+                  key={index}
+                  onClick={() => {
+                    setSelectedType(database.name);
+                  }}
+                >
+                  <ImageCard
+                    image={database.image}
+                    selected={selectedType === database.name}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col items-start gap-6">
+            <p className="text-[17px] font-medium leading-7">Cloud Storage</p>
+            <div className="flex gap-6 items-start">
+              {data.cloud.map((storage, index) => (
+                <div
+                  key={index}
+                  onClick={() => {
+                    setSelectedType(storage.name);
+                  }}
+                >
+                  <ImageCard
+                    image={storage.image}
+                    selected={selectedType === storage.name}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      ),
+    },
+    {
+      number: 2,
+      title: "Upload File",
+      content: (
+        <section className="px-7 py-10 flex gap-[40px]">
+          <div className="flex flex-col gap-6 items-start text-sm font-medium">
+            <div className="flex gap-[10px]">
+              <div className="flex items-center gap-[10px] text-[#17212F]">
+                <input
+                  type="radio"
+                  name="new"
+                  id="new"
+                  className="accent-black"
+                />
+                <span>Upload a new file</span>
+              </div>
+              <p className="text-[#ADB3BB] underline">File Guideline</p>
+            </div>
+            <div className="flex w-[384px] gap-2 items-start">
+              <div className="flex flex-col gap-[6px] flex-[1_0_0] items-start rounded">
+                <div className="flex px-3 py-2 items-center self-stretch rounded-[6px] border border-[#EAEDF2] bg-white">
+                  <p className="text-[#ADB3BB]">File name</p>
+                </div>
+              </div>
+              <div>
+                <input type="file" className="hidden" id="fileInput" />
+                <label
+                  htmlFor="fileInput"
+                  className="flex justify-center items-center gap-[10px] rounded px-4 py-2 text-sm bg-blue-500 text-white cursor-pointer"
+                >
+                  Browse
+                </label>
+              </div>
+            </div>
+            <div className="flex items-start gap-[10px]">
+              <div className="flex items-center gap-[10px] text-[#17212F]">
+                <input
+                  type="radio"
+                  name="new"
+                  id="new"
+                  className="accent-black"
+                />
+                <span>Select a previously uploaded file</span>
+              </div>
+              <p className="text-[#ADB3BB] underline">Manage File</p>
+            </div>
+            <Popover>
+              <PopoverTrigger className="flex items-center gap-[10px] px-3 py-2 w-[204px] rounded bg-white border border-[#EAEDF2]">
+                <span className="flex-[1_0_0] text-start">Select a file</span>
+                <Image
+                  src="/assets/chevron-down.svg"
+                  alt="chevron down icon"
+                  width={16}
+                  height={16}
+                />
+              </PopoverTrigger>
+              <PopoverContent className="w-[204px] bg-white">
+                <ul className="text-sm">
+                  <li className="flex gap-2 items-center px-3 py-[6px] cursor-pointer hover:bg-[#F8FAFC] rounded">
+                    Data set
+                  </li>
+                  <li className="flex gap-2 items-center px-3 py-[6px] cursor-pointer hover:bg-[#F8FAFC] rounded">
+                    Data set
+                  </li>
+                  <li className="flex gap-2 items-center px-3 py-[6px] cursor-pointer hover:bg-[#F8FAFC] rounded">
+                    Data set
+                  </li>
+                </ul>
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div className="w-px h-[200px] rounded-[10px] bg-[#DEE8FA]"></div>
+          <div className="flex flex-col gap-6">
+            <div className="flex items-start gap-[10px] text-sm font-medium">
+              <div className="flex items-center gap-2 text-[#17212F]">
+                <input
+                  type="radio"
+                  name="new"
+                  id="new"
+                  className="accent-black"
+                />
+                <span>Create new dataset</span>
+              </div>
+            </div>
+            <div className="flex items-start gap-[10px] text-sm font-medium">
+              <div className="flex items-center gap-2 text-[#17212F]">
+                <input
+                  type="radio"
+                  name="new"
+                  id="new"
+                  className="accent-black"
+                />
+                <span>Add to an existing dataset</span>
+              </div>
+            </div>
+            <Popover>
+              <PopoverTrigger className="flex items-center gap-[10px] px-3 py-2 w-[204px] rounded bg-white border border-[#EAEDF2]">
+                <span className="flex-[1_0_0] text-start text-sm">
+                  Select a file
+                </span>
+                <Image
+                  src="/assets/chevron-down.svg"
+                  alt="chevron down icon"
+                  width={16}
+                  height={16}
+                />
+              </PopoverTrigger>
+              <PopoverContent className="w-[204px] bg-white">
+                <ul className="text-sm">
+                  <li className="flex gap-2 items-center px-3 py-[6px] cursor-pointer hover:bg-[#F8FAFC] rounded">
+                    {true ? (
+                      <Image
+                        src="/assets/check-icon-black.svg"
+                        alt="down icon"
+                        width={16}
+                        height={16}
+                      />
+                    ) : (
+                      <p className="w-4"></p>
+                    )}
+                    Data set
+                  </li>
+                  <li className="flex gap-2 items-center px-3 py-[6px] cursor-pointer hover:bg-[#F8FAFC] rounded">
+                    {false ? (
+                      <Image
+                        src="/assets/check-icon-black.svg"
+                        alt="down icon"
+                        width={16}
+                        height={16}
+                      />
+                    ) : (
+                      <p className="w-4"></p>
+                    )}
+                    Data set
+                  </li>
+                  <li className="flex gap-2 items-center px-3 py-[6px] cursor-pointer hover:bg-[#F8FAFC] rounded">
+                    {false ? (
+                      <Image
+                        src="/assets/check-icon-black.svg"
+                        alt="down icon"
+                        width={16}
+                        height={16}
+                      />
+                    ) : (
+                      <p className="w-4"></p>
+                    )}
+                    Data set
+                  </li>
+                </ul>
+              </PopoverContent>
+            </Popover>
+            <div className="flex items-start gap-[10px] text-sm font-medium text-[#ADB3BB]">
+              <p>Uploaded file has to have same schema as the existing data</p>
+            </div>
+          </div>
+        </section>
+      ),
+    },
+    { number: 3, title: "Select Table", content: <p>Hello</p> },
+    { number: 4, title: "Edit fields", content: <p>Hello</p> },
+    { number: 5, title: "Add Dataset Info", content: <p>Hello</p> },
+  ];
+
   return (
     <div>
       <div className="h-[49px] px-7 py-3 flex justify-between items-center bg-[#DEE8FA]">
@@ -66,63 +291,8 @@ const Page = () => {
           ))}
         </div>
       </div>
-      <section className="px-7 py-10 flex flex-col items-start gap-6">
-        <div className="flex flex-col items-start gap-6">
-          <p className="text-[17px] font-medium leading-7">Files</p>
-          <div className="flex gap-6 items-start">
-            {data.files.map((file, index) => (
-              <div
-                key={index}
-                onClick={() => {
-                  setSelectedType(file.name);
-                }}
-              >
-                <ImageCard
-                  image={file.image}
-                  selected={selectedType === file.name}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-col items-start gap-6">
-          <p className="text-[17px] font-medium leading-7">Databases</p>
-          <div className="flex gap-6 items-start">
-            {data.databases.map((database, index) => (
-              <div
-                key={index}
-                onClick={() => {
-                  setSelectedType(database.name);
-                }}
-              >
-                <ImageCard
-                  image={database.image}
-                  selected={selectedType === database.name}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-col items-start gap-6">
-          <p className="text-[17px] font-medium leading-7">Cloud Storage</p>
-          <div className="flex gap-6 items-start">
-            {data.cloud.map((storage, index) => (
-              <div
-                key={index}
-                onClick={() => {
-                  setSelectedType(storage.name);
-                }}
-              >
-                <ImageCard
-                  image={storage.image}
-                  selected={selectedType === storage.name}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-      <Footer />
+      {steps[currentStep - 1].content}
+      <Footer step={currentStep} />
     </div>
   );
 };
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,23 @@
-import Button from "./Button";
+import Link from "next/link";
 
-const Footer = () => {
+type Props = {
+  step: number;
+};
+
+const Footer = ({ step }: Props) => {
   return (
-    <div className="flex px-7 flex-col items-start gap-6 pb-4">
+    <div className="fixed bottom-[12px] left-16 right-7 flex px-7 flex-col items-start gap-6 pb-[54px]">
       <div className="flex p-3 justify-between items-center self-stretch rounded border border-[#EAEDF2] bg-white">
         <div className="flex justify-between items-center flex-[1_0_0]">
-          <button className="px-4 py-2 rounded border border-[#DEE8FA]">
-            Cancel
-          </button>
+          <Link href="/dashboard/data">
+            <button className="px-4 py-2 rounded border border-[#DEE8FA]">
+              Cancel
+            </button>
+          </Link>
           <div className="flex items-center gap-3">
-            <button className="px-4 py-2 rounded bg-[#DEE8FA]">Back</button>
+            {step > 1 && (
+              <button className="px-4 py-2 rounded bg-[#DEE8FA]">Back</button>
+            )}
             <button className="px-4 py-2 rounded bg-blue-500 text-white">
               Next
             </button>
