b21b86d169ae363639d9f32ce987ededcc1d113f
logan-exe <arun.11143@gmail.com>
2023-11-14 17:07:45 +0000

    [fix] redirect to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent, FormEvent, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import Loading from "@/components/Loading";
@@ -8,7 +8,11 @@ import Loading from "@/components/Loading";
 export default function Home() {
   const router = useRouter();
   const [file, setFile] = useState<File | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    router.push("/user/login");
+  }, []);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -45,22 +49,23 @@ export default function Home() {
       {isLoading ? (
         <Loading />
       ) : (
-        <main className="flex min-h-screen flex-col items-center justify-center">
-          <form onSubmit={handleSubmit}>
-            <h1 className="text-2xl font-medium mb-4">Upload a file</h1>
-            <div className="flex gap-4">
-              <input
-                type="file"
-                accept=".csv"
-                onChange={handleFileChange}
-                className="bg-gray-700 p-2 text-white"
-              />
-              <button className="bg-primary rounded-md text-white px-4 h-12">
-                Upload
-              </button>
-            </div>
-          </form>
-        </main>
+        <></>
+        // <main className="flex min-h-screen flex-col items-center justify-center">
+        //   <form onSubmit={handleSubmit}>
+        //     <h1 className="text-2xl font-medium mb-4">Upload a file</h1>
+        //     <div className="flex gap-4">
+        //       <input
+        //         type="file"
+        //         accept=".csv"
+        //         onChange={handleFileChange}
+        //         className="bg-gray-700 p-2 text-white"
+        //       />
+        //       <button className="bg-primary rounded-md text-white px-4 h-12">
+        //         Upload
+        //       </button>
+        //     </div>
+        //   </form>
+        // </main>
       )}
     </div>
   );
